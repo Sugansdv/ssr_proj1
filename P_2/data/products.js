865a8c8aa4afa2f7e977a9baeb509f8da790f439
Sugansdv <suganyasdv16@gmail.com>
2025-06-20 14:33:33 +0530

    proj1 to proj4 folders

--- P_2/data/products.js
+++ P_2/data/products.js
@@ -1,7 +1,7 @@
 const products = [
-  { id: '1', name: 'iPhone 15', price: '$999', description: 'Latest Apple iPhone 15 with A16 chip.' },
-  { id: '2', name: 'Samsung Galaxy S23', price: '$899', description: 'Flagship Samsung phone with Android 13.' },
-  { id: '3', name: 'OnePlus 12', price: '$799', description: 'Fast and smooth OnePlus 12 with Snapdragon.' },
+  { id: '1', name: 'iPhone 15', price: 'Rs30999', description: 'Latest Apple iPhone 15 with A16 chip.' },
+  { id: '2', name: 'Samsung Galaxy S23', price: 'Rs.56899', description: 'Flagship Samsung phone with Android 13.' },
+  { id: '3', name: 'OnePlus 12', price: 'Rs.25799', description: 'Fast and smooth OnePlus 12 with Snapdragon.' },
 ];
 
 export default products;
